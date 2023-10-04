b9bc7c549144bee0583a8489457238d6ab5e7fdc
eroar <48735008+Eroar@users.noreply.github.com>
2023-10-04 22:56:17 +0200

    Silence errors for non serializable values

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,6 +6,10 @@ export const store = configureStore({
 	reducer: {
 		pdf: pdfReducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
