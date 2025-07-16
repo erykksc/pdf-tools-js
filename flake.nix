62ec1a99dfd6e5cc5c4093c9665c8221e38ef6de
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-07-16 16:44:05 +0200

    chore(flake.nix): add devEnv

--- flake.nix
+++ flake.nix
@@ -3,15 +3,27 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = inputs:
+  outputs =
+    inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [ inputs.self.overlays.default ];
-        };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+            system = system;
+          }
+        );
     in
     {
       overlays.default = final: prev: rec {
@@ -19,10 +31,28 @@
         yarn = (prev.yarn.override { inherit nodejs; });
       };
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ node2nix nodejs nodePackages.pnpm yarn ];
-        };
-      });
+      packages = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          devEnv = pkgs.buildEnv {
+            name = "pdf-tools-js-env";
+            paths = [
+              pkgs.nodejs
+              pkgs.nixfmt-rfc-style
+            ];
+          };
+        }
+      );
+
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              inputs.self.packages.${system}.devEnv
+            ];
+          };
+        }
+      );
     };
 }
